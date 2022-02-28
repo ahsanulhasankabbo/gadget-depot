1c458dddf69fd4082647a5b597ddb323aa042f03
Ahsanul Hasan Kabbo <kabboahsanulhasan@gmail.com>
2022-03-01 02:01:37 +0600

    details add

--- js/api.js
+++ js/api.js
@@ -6,10 +6,13 @@ const loadPhone = () => {
     fetch(url)
     .then(response => response.json())
     .then(data => displayPhone(data.data))
+
+    searchField.value = '';
 }
 
 const displayPhone = phones => {
     const phoneContainer = document.getElementById('phone-container');
+    phoneContainer.textContent = '';
     phones.forEach(phone =>{
         // console.log(phone)
         const div = document.createElement('div');
@@ -18,11 +21,39 @@ const displayPhone = phones => {
                   <div class="card">
                     <img src="${phone.image}" class="card-img-top w-50 mx-auto" alt="...">
                     <div class="card-body text-center">
-                      <h3 class="card-title">${phone.phone_name}</h3>
-                      <h3 class="card-text">${phone.brand}</h3>
+                      <h5 class="card-title">Name : ${phone.phone_name}</h5>
+                      <h5 class="card-text">Brand : ${phone.brand}</h5>
+                      <button class="btn bg-success text-white" onclick="phoneDetails('${phone.slug}')">Details</button>
                     </div>
+                    
                   </div>
         `
         phoneContainer.appendChild(div);
     })
+}
+
+const phoneDetails = (info) => {
+    const url = `https://openapi.programming-hero.com/api/phone/${info}`;
+    fetch(url)
+    .then(response => response.json())
+    .then(data => displayDetails(data.data))
+}
+
+const displayDetails = infoId => {
+    // console.log(infoId);
+    const detailsPhone = document.getElementById('phone-details');
+    detailsPhone.textContent = '';
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+    <img src="${infoId.image}" class="card-img-top" alt="...">
+    <div class="card-body">
+        <p class="card-text"><span class="fw-bold">Name :</span> ${infoId.name}</p>
+        <p class="card-text"><span class="fw-bold">Release Date :</span> ${infoId.releaseDate}</p>
+        <p class="card-text"><span class="fw-bold">Storage :</span> ${infoId.mainFeatures.storage}</p>
+        <p class="card-text"><span class="fw-bold">Memory Capacity :</span> ${infoId.mainFeatures.memory}</p>
+        <p class="card-text"><span class="fw-bold">Display :</span> ${infoId.mainFeatures.displaySize}</p>
+    </div>
+    `
+    detailsPhone.appendChild(div);
 }
