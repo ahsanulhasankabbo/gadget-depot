372e335e7b968e40dc33276d4e16fe23a99bea33
Ahsanul Hasan Kabbo <kabboahsanulhasan@gmail.com>
2022-03-01 13:06:07 +0600

    responsive design show

--- js/api.js
+++ js/api.js
@@ -9,15 +9,19 @@ const loadPhone = () => {
     // display spinner 
     loadSpinner('block');
 
+    // phone url 
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`;
     fetch(url)
     .then(response => response.json())
     .then(data => displayPhone(data.data))
 
+    // empty search field 
     searchField.value = '';
 }
 
+// display phones 
 const displayPhone = phones => {
+    // console.log(phones)
     const phoneContainer = document.getElementById('phone-container');
     const detailsPhone = document.getElementById('phone-details');
     
@@ -30,13 +34,13 @@ const displayPhone = phones => {
     }
     // display function 
     else{
-    phones.forEach(phone =>{
+    phones.slice(0,20).forEach(phone =>{
         document.getElementById('error-section').style.display = 'none';
         // console.log(phone)
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-                  <div class="card">
+                  <div class="card border-0 shadow">
                     <img src="${phone.image}" class="card-img-top w-50 mx-auto" alt="...">
                     <div class="card-body text-center">
                       <h5 class="card-title">Name : ${phone.phone_name}</h5>
@@ -52,6 +56,7 @@ const displayPhone = phones => {
     }
 }
 
+// display details 
 const phoneDetails = (info) => {
     const url = `https://openapi.programming-hero.com/api/phone/${info}`;
     fetch(url)
@@ -62,12 +67,13 @@ const phoneDetails = (info) => {
 const displayDetails = infoId => {
     console.log(infoId);
     const detailsPhone = document.getElementById('phone-details');
-    // detailsPhone.textContent = '';
     const div = document.createElement('div');
     div.classList.add('card');
+    div.classList.add('border-0');
     div.innerHTML = `
-    <img src="${infoId.image}" class="card-img-top" alt="...">
-    <div class="card-body">
+    <div class="shadow">
+    <img src="${infoId.image}" class="card-img-top p-5" alt="...">
+        <div class="card-body">
         <p class="card-text"><span class="fw-bold">Name :</span> ${infoId.name}</p>
         <p class="card-text"><span class="fw-bold">Release Date :</span> ${infoId.releaseDate!==''?infoId.releaseDate:'<span class="text-danger fw-bold">not found</span>'}</p>
         <p class="card-text"><span class="fw-bold">Storage :</span> ${infoId.mainFeatures.storage}</p>
@@ -75,11 +81,12 @@ const displayDetails = infoId => {
         <p class="card-text"><span class="fw-bold">Display :</span> ${infoId.mainFeatures.displaySize}</p>
         <p class="card-text"><span class="fw-bold">Sensors :</span> ${infoId.mainFeatures.sensors}</p>
         <p class="m-0"><span class="fw-bold">Others : </span></p>
-        <p class="card-text m-0"><span class="fw-bold"></span> ${infoId.others.WLAN}</p>
-        <p class="card-text m-0"><span class="fw-bold"></span> ${infoId.others.Bluetooth}</p>
-        <p class="card-text m-0"><span class="fw-bold"></span> ${infoId.others.GPS}</p>
-        <p class="card-text m-0"><span class="fw-bold"></span> ${infoId.others.USB}</p>
+        <p class="card-text m-0">WALN : ${infoId.others.WLAN}</p>
+        <p class="card-text m-0">Bluetooth : ${infoId.others.Bluetooth}</p>
+        <p class="card-text m-0">GPS : ${infoId.others.GPS}</p>
+        <p class="card-text m-0">USB : ${infoId.others.USB}</p>
 
+        </div>
     </div>
     `
     detailsPhone.appendChild(div);
